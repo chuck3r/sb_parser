d23f9acb4dd799967114834eece92a3dcdce8449
rutch <rachit.chakerwarti>
2016-08-15 20:40:29 -0400

    thanks karo

---
+++ scotia_chequing_org.py
@@ -0,0 +1,27 @@
+import csv
+
+output_file = open("banking_output.csv", "w")
+writer = csv.writer(output_file)
+
+
+with open('pcbanking.csv', 'rb') as csvfile:
+    data = csv.reader(csvfile)
+    for row in data:
+        if row[1][0] == '-' and row[3] != "ABM Withdrawal":
+            date = row[0]
+            amount = row[1][1:]
+            description = " ".join(row[4].lower().split())
+            writer.writerow([date, amount, description])
+
+with open('pcbanking.csv', 'rb') as csvfile:
+    data = csv.reader(csvfile)
+    for row in data:
+        if row[1][0] != '-':
+            date = row[0]
+            amount = row[1]
+            description1 = " ".join(row[3].lower().split())
+            description = " ".join(row[4].lower().split())
+            writer.writerow([date, amount, description, description1])
+
+output_file.close()
+    
